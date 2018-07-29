c22fbbf380a147ffa18b817e7b4e6bf2f0b1da6b
Ariana Baseman <ArianaMB@yahoo.com>
2018-07-29 19:50:54 -0400

    JavaScript_Slide_33

---
+++ longerName.js
@@ -0,0 +1,8 @@
+const name1 = "Jonathan Price"
+const name2 = "Jonathan Longer"
+
+if (name1.length > name2.length){
+console.log (`${name1} has the longer name.`);
+} else if (name2.length > name1.length){
+console.log (`${name2} has the longer name.`);
+}
